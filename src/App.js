b7f2f0b5a7ea3d49eca52fb0ba50d7e934f22a39
surendrakumarperumalla <perumallasurendra12345@gmail.com>
2024-05-23 16:19:51 +0530

    formik-advance

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Studentform from './studentform';
 
+
 function App() {
   return (
    <div>
     <h1>App</h1>
- <Studentform></Studentform>
+    <Studentform></Studentform>
+   
     </div>
   );
 }
