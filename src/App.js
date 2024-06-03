746c665c7997e5e7333fed8d81d2920cd204f1fe
surendrakumarperumalla <perumallasurendra12345@gmail.com>
2024-06-03 17:50:33 +0530

    dfgh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import React from 'react';
 
 function App() {
-  return (
+  var [product,setProduct]=useState([])
+  var[details,setDetails]=useState([])
+  useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products').then((res)=>{
+        setProduct(res.data)    
+    },[])
+  })
+  function expand(id){
+    setDetails(id)
+  }
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+      product?.map((p)=>{
+        return<div >
+        <li className='bg-dark text-light m-2 p-2 k'>
+          <div className='d-flex justify-content-between'>
+          {p.title}
+          <i class="bi bi-plus-circle"  onClick={()=>{expand(p.id)}}></i>
+          <i class="bi bi-dash-circle" onClick={()=>{}}></i>
+          </div>
+
+        <h1 style={p.id===details?{display:"block"}:{display:"none"}}>
+          DETAILS <br />
+          <img  src={p.image} className='w-25'/>
+        </h1> 
+             </li>
+    </div>
+      })
+    }
     </div>
   );
 }
