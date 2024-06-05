6e492d48c0acf1be3197fd5548be7dc3de82c9fd
surendrakumarperumalla <perumallasurendra12345@gmail.com>
2024-06-05 11:05:27 +0530

    gh

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <h1><span>A</span></h1>
+        <h1><span>B</span></h1>
+        <h1><span>C</span></h1>
+     
     </div>
   );
 }
